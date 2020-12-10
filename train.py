779a0b30304032e1bed29f6664cfffe72f0df8d6
thomas-waite <thomas.waite4@gmail.com>
2020-12-10 00:56:03 +0000

    refactor: move fns around, retrain

--- train.py
+++ train.py
@@ -1,32 +1,12 @@
-from analysis import generate_plot
+from utils import generate_plot, load_data, MODEL_PATH
 from model import model_factory
-import numpy as np
-import pandas as pd
 from keras.models import load_model
-
-MODEL_PATH = './weights/model'
-
-
-def load_data():
-    dataset = pd.read_csv('pima-indians-diabetes.csv')
-    raw_data = np.loadtxt('pima-indians-diabetes.csv', delimiter=',')
-
-    # split into input and output variables
-    input_dim = 8
-    X = raw_data[:, 0:input_dim]
-    y = raw_data[:, input_dim]
-
-    return X, y, input_dim
 
 
 def train(data, labels, model):
     history = model.fit(data, labels, epochs=150, batch_size=10)
     generate_plot(history, 'loss')
     model.save(MODEL_PATH)
-
-
-def load_model():
-    return load_model(MODEL_PATH)
 
 
 X, y, input_dim = load_data()
---
+++ utils.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from keras.models import Sequential, load_model
+from typing import Tuple
+
+MODEL_PATH: str = './weights/model'
+
+
+def generate_plot(history, model_var: str):
+    plt.plot(history.history[model_var])
+    plt.plot(history.history[f'{model_var}'])
+    plt.title('model loss')
+    plt.ylabel(model_var)
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+
+def load_data() -> Tuple[list, list, int]:
+    dataset = pd.read_csv('pima-indians-diabetes.csv')
+    raw_data = np.loadtxt('pima-indians-diabetes.csv', delimiter=',')
+
+    # split into input and output variables
+    input_dim = 8
+    X = raw_data[:, 0:input_dim]
+    y = raw_data[:, input_dim]
+
+    return X, y, input_dim
+
+
+def load_keras_model():
+    return load_model(MODEL_PATH)
