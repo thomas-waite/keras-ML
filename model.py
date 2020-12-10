798464f146bdf66062ffe90b115a886b13218437
thomas-waite <thomas.waite4@gmail.com>
2020-12-10 00:06:15 +0000

    feat: add basic plotting fn

--- model.py
+++ model.py
@@ -3,17 +3,13 @@ from keras.layers import Dense
 from keras.utils.vis_utils import plot_model
 
 
-def create_model(input_dim: int) -> Sequential:
+def model_factory(input_dim: int) -> Sequential:
     model = Sequential()
     model.add(Dense(12, input_dim=input_dim, activation='relu'))
     model.add(Dense(8, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy',
                   optimizer='adam', metrics=['accuracy'])
-    print(model.summary())
     plot_model(model, to_file='model_plot.png',
                show_shapes=True, show_layer_names=True)
     return model
-
-
-model = create_model(8)
